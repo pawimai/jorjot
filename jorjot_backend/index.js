a04a7f8439c1bc6671d48b6ca8f7b0c1afad8052
natnichaxxx <natnib.xx@gmail.com>
2025-02-25 14:15:55 +0700

    edit register

--- jorjot_backend/index.js
+++ jorjot_backend/index.js
@@ -229,4 +229,4 @@ app.use('/', (req,res) => {
     res.status(404).send('<h1>404 Page Not Found!</h1>');
 });
 
-app.listen(3000, () => console.log("Server is running..."));
+app.listen(3001, () => console.log("Server is running..."));
