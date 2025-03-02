da20853122c1df61107800bbd2dbe7b1df369b40
natnichaxxx <natnib.xx@gmail.com>
2025-03-02 20:09:59 +0700

    Update Navbar.tsx

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,17 +1,38 @@
+"use client"
 import InsertInvitationIcon from "@mui/icons-material/InsertInvitation";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import Swal from "sweetalert2";
-import config from "../../config";
+import config from "../config";
 import Cookies from "js-cookie";
 
-
 export default function Nav() {
+    const [profileImage, setProfileImage] = useState("/defaultProfile.jpg"); // Default profile image
 
+    useEffect(() => {
+        const fetchProfileImage = async () => {
+            try {
+                const token = Cookies.get('token');
 
-    
+                const response = await axios.get(config.api_path + "/auth/profile", {
+                    headers: {
+                        Authorization: `${token}`
+                    }
+                });
+
+                if (response.status === 200) {
+                    const data = response.data;
+                    setProfileImage(data.profileImage); // Update state with fetched profile image
+                }
+            } catch (error) {
+                console.error("Error fetching profile image:", error);
+            }
+        };
+
+        fetchProfileImage();
+    }, []);
+
     return (
         <nav className="flex items-end justify-between h-[14vh] bg-[#4C3228] rounded-b-[30px] pb-3 px-6 ">
             {/* โลโก้ชื่อเว็บ */}
@@ -28,14 +49,13 @@ export default function Nav() {
                 <Link href={"/profile"}>
                     <div className="relative w-10 h-10 overflow-hidden border-[3px] border-[#FCDD45] rounded-full bg-white">
                         <Image
-                            src="/profile1.jpg" //เปลี่ยนรูปโปรไฟล์ได้
+                            src={profileImage} // Use fetched profile image
                             alt="Profile"
                             fill
                             className="object-cover"
                         />
                     </div>
                 </Link>
-
             </div>
         </nav>
     );
